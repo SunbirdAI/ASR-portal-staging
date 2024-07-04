31239a278f3e0c0b7096378af2ea336ad9e8df92
evie-8 <neouma29@gmail.com>
2024-07-04 17:43:43 +0300

    resolving branch conflicts

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,13 @@
-import {Nav, Title, Logo} from "./Header.styles";
+import { Nav, Title, Logo } from "./Header.styles";
 import { Dropdown } from "../DropDown";
-import img from '../../images/logo.png';
-
+import img from "../../images/logo.png";
 
 const Header = () => (
-    <Nav >
-        <Logo alt="Logo" src={img}/>
-        <Title>
-            Speech To Text.
-        </Title>       
-        <Dropdown /> 
-    </Nav>
+  <Nav>
+    <Logo alt="Logo" src={img} />
+    <Title>Speech To Text.</Title>
+    <Dropdown />
+  </Nav>
 );
 
 export default Header;
