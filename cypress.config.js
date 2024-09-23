305effe34a3875f3ec2cbc214dd34b80a012c80a
gilbertyiga <vigidaiz15@gmail.com>
2024-09-23 14:50:47 +0300

    Added my component based tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  chromeWebSecurity: false,
+
   e2e: {
+    baseUrl: "http://localhost:3000",
+    env: {
+      localUrl: "http://localhost:3000",
+      productionUrl: "https://speech.sunbird.ai",
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "create-react-app",
+      bundler: "webpack",
     },
   },
 });
